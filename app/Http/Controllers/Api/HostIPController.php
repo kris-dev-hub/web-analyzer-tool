8059cc66aa7f3ce14a169dfd94419d8197be4bb3
kris-dev-hub <kris@kris.biz.pl>
2024-01-12 18:48:54 +0100

    feat: OpenAPI config

--- app/Http/Controllers/Api/HostIPController.php
+++ app/Http/Controllers/Api/HostIPController.php
@@ -5,10 +5,49 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Info(
+ *     version="0.0.1",
+ *     title="Web analyze tool",
+ *     description="Tool to analyze webside information",
+ *     @OA\Contact(
+ *         email="kris@kris.biz.pl"
+ *     )
+ * )
+ *
+ * @OA\Server(
+ *     description="API Server",
+ *     url="http://localhost:8001/api"
+ * )
+ */
+
+
 class HostIPController extends Controller
 {
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *     path="/api/hostip/{host}",
+     *     operationId="getHostIp",
+     *     tags={"HostIP"},
+     *     summary="Get IP details for the host",
+     *     description="Returns IP info for a specific host",
+     *     @OA\Parameter(
+     *         name="host",
+     *         description="Host name",
+     *         required=true,
+     *         in="path",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/HostIP")
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
      */
     public function show(string $host)
     {
--- app/Models/HostIP.php
+++ app/Models/HostIP.php
@@ -5,6 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="HostIP",
+ *     type="object",
+ *     @OA\Property(
+ *         property="ip",
+ *         type="string",
+ *         description="The IP address of the host",
+ *     ),
+ *     @OA\Property(
+ *         property="hostname",
+ *         type="string",
+ *         description="The hostname of the IP address",
+ *     )
+ * )
+ */
 class HostIP extends Model
 {
     use HasFactory;
