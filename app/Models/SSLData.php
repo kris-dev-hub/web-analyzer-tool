e9fe254b8688cfad7729f67b7593cf14d2d25420
kris-dev-hub <kris@kris.biz.pl>
2024-01-13 15:24:00 +0100

    feat: OpenAPI config for new api call

--- app/Models/SSLData.php
+++ app/Models/SSLData.php
@@ -12,18 +12,78 @@ use Illuminate\Database\Eloquent\Model;
  *     @OA\Property(
  *         property="status",
  *         type="string",
- *         description="Status of the SSL certificate",
+ *         description="Status of the SSL certificate"
  *     ),
  *     @OA\Property(
  *         property="issuer",
  *         type="string",
- *         description="Issuer",
+ *         description="Issuer of the SSL certificate"
  *     ),
  *     @OA\Property(
- *          property="valid",
- *          type="bool",
- *          description="Is cerfiticate valid",
- *      ),
+ *         property="valid",
+ *         type="boolean",
+ *         description="Indicates whether the certificate is valid"
+ *     ),
+ *     @OA\Property(
+ *         property="signature_algorithm",
+ *         type="string",
+ *         description="Signature algorithm of the SSL certificate"
+ *     ),
+ *     @OA\Property(
+ *         property="fingerprint",
+ *         type="string",
+ *         description="Fingerprint of the SSL certificate"
+ *     ),
+ *     @OA\Property(
+ *         property="additional_domains",
+ *         type="array",
+ *         @OA\Items(
+ *             type="string"
+ *         ),
+ *         description="Additional domains included in the SSL certificate"
+ *     ),
+ *     @OA\Property(
+ *         property="expired",
+ *         type="boolean",
+ *         description="Indicates whether the SSL certificate is expired"
+ *     ),
+ *     @OA\Property(
+ *         property="self_signed",
+ *         type="boolean",
+ *         description="Indicates whether the SSL certificate is self-signed"
+ *     ),
+ *     @OA\Property(
+ *         property="valid_from",
+ *         type="object",
+ *         @OA\Property(
+ *             property="date",
+ *             type="string",
+ *             format="datetime",
+ *             description="Date from which the SSL certificate is valid"
+ *         ),
+ *         @OA\Property(
+ *             property="timezone",
+ *             type="string",
+ *             description="Timezone of the valid from date"
+ *         ),
+ *         description="Information about the start date of the SSL certificate's validity"
+ *     ),
+ *     @OA\Property(
+ *         property="expiration_date",
+ *         type="object",
+ *         @OA\Property(
+ *             property="date",
+ *             type="string",
+ *             format="datetime",
+ *             description="Date on which the SSL certificate expires"
+ *         ),
+ *         @OA\Property(
+ *             property="timezone",
+ *             type="string",
+ *             description="Timezone of the expiration date"
+ *         ),
+ *         description="Information about the expiration date of the SSL certificate"
+ *     )
  * )
  */
 class SSLData extends Model
